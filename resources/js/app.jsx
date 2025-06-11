67e48d4b800076cde7b74f190b4e5bbec0895784
ameen <ameen@gmail.com>
2025-06-11 11:03:58 +0300

    change some css of about us and header

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -14,18 +14,20 @@ const App = () => {
 
     return (
         <div dir={lang === 'ar' ? 'rtl' : 'ltr'}>
-            <div className="relative min-h-screen w-full overflow-hidden bg-gradient-to-br from-primary via-shining to-secondary text-white">
+            <div className="relative w-full overflow-hidden bg-gradient-to-br from-primary via-shining to-secondary text-white">
                 {/* Abstract Shapes */}
                 <div className="absolute top-0 left-0 w-72 h-72 bg-shining rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
                 <div className="absolute top-0 right-0 w-72 h-72 bg-secondary rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
                 <div className="absolute bottom-20 left-20 w-72 h-72 bg-primary rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
 
-                <div className="relative z-10 p-4 sm:p-6 lg:p-8">
-                    <Header lang={lang} toggleLanguage={toggleLanguage} />
-                    <Hero lang={lang} />
+                <div className="relative z-10">
+                    <div className="p-4 sm:p-6 lg:p-8">
+                        <Header lang={lang} toggleLanguage={toggleLanguage} />
+                        <Hero lang={lang} />
+                    </div>
+                    <AboutUs lang={lang} />
                 </div>
             </div>
-            <AboutUs lang={lang} />
         </div>
     );
 };
